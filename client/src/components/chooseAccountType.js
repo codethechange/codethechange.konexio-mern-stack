dcd7e3deb7376d13dc5403b7d2d54fd397d86af6
Eric Zhou <ericzhou@stanford.edu>
2019-04-05 19:54:55 -0700

    Specific paths for mentor and mentee signup
    Added new paths for mentor and mentee signup. Now when you click signup, it asks you what type of account you wish to make before redirecting you to the appropriate form path.

---
+++ client/src/components/chooseAccountType.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { WelcomeButton } from "./welcomeButton";
+
+class ChooseAccountType extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomeButton />
+      </div>
+    );
+  }
+}
+
+export default ChooseAccountType;
