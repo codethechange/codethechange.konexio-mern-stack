2b8484c0b5562c9b621fdb4c783e4b5c0df95441
Anthony Perez <anthonyacostaperez@gmail.com>
2019-04-15 21:31:30 -0700

    Config.js update

--- server/src/config.js
+++ server/src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 if(process.env.NODE_ENV != 'production'){
-  dotenv.config({ path: path.resolve(__dirname, 'dot.env') });
+  dotenv.config({ path: path.resolve(__dirname, '.env') });
 }
 
 module.exports = {
