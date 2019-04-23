e7e676d3e53fe090f7fd9e3481eab80075adbcff
i-gallegos <iogalle@stanford.edu>
2019-04-22 22:57:52 -0700

    added front end components to myConnections

--- client/src/components/myConnections.js
+++ client/src/components/myConnections.js
@@ -1,11 +1,66 @@
-import React, { Component } from 'react';
+// sign in, the my connections OR /public
 
-export default class Public extends Component {
+import React, { Component } from 'react';
+//import CenterCard363 from "../centerCard363";
+
+class MyConnections extends Component {
   render() {
     return (
-      <div>
-      <h1>My Connections</h1>
+      //<CenterCard363>
+      <div class="container-fluid">
+      	<div class="row">
+      		<div class="col-md-12">
+      			<div class="jumbotron">
+      				<h2>
+      					Welcome
+      				</h2>
+      				<p>
+      					Meet your mentor here.
+      				</p>
+      			</div>
+      			<div class="row">
+              <div class="col-md-2">
+              </div>
+      				<div class="col-md-4">
+      					<img alt="Bootstrap Image Preview" src="../images/profile_pic.png" />
+      				</div>
+              <div class="col-md-1">
+              </div>
+      				<div class="col-md-5">
+      					<h3>
+      						FirstName LastName
+      					</h3>
+      					<p>
+      						<strong>Company Name</strong><br/>
+                  <strong>Email:</strong> email@example<br/>
+                  <strong>Phone Number:</strong>XXX-XXX-XXXX<br/>
+      					</p>
+      				</div>
+      			</div>
+            <div class="row">
+            </div>
+      			<div class="row">
+              <div class="col-md-2">
+              </div>
+      				<div class="col-md-3">
+      					<button type="button" class="btn btn-primary btn-lg btn-block">
+      						Learn More
+      					</button>
+      				</div>
+              <div class="col-md-2">
+              </div>
+      				<div class="col-md-3">
+      					<button type="button" class="btn btn-primary btn-block btn-lg">
+      						Contact Now
+      					</button>
+      				</div>
+              <div class="col-md-2">
+              </div>
+      			</div>
+      		</div>
+      	</div>
       </div>
+      //</CenterCard363>
     );
   }
 }
