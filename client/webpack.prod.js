a9397f72ec6f3212f57fbb938b25416ea1ee1a62
farzaank <fkaiyom@gmail.com>
2019-05-24 18:40:41 -0700

    something new

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'API_URI': JSON.stringify('https://konexio-mentoring-platform.herokuapp.com')
+                'API_URI': JSON.stringify('https://konexioserver.herokuapp.com')
             },
         }),
     ]
